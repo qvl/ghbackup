079e89c135ce7484e0103304c4de4ed7409d4efb
Jorin Vogel <hi@jorin.me>
2016-08-02 11:42:27 +0200

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+# github-backup
+
+An embarrassing simple Github backup tool
+
+
+    Usage: github-backup user backupdir
+
+    user       github user name to get the repositories from
+    backupdir  directory path to save the repositories to
+
+
+
+### What happens?
+
+Get all repositories of a Github user.
+Save them to folder.
+Update already cloned repositories.
+
+Best served as a scheduled job to keep your backups up to date!
---
+++ main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
+	"sync"
+)
+
+var usage = `Usage: %s user backupdir
+
+user       github user name to get the repositories from
+backupdir  directory path to save the repositories to`
+
+type Repo struct {
+	Name   string
+	GitUrl string `json:"git_url"`
+}
+
+func main() {
+	user, backupDir := parseArgs()
+
+	repos := getRepos(fmt.Sprint("https://api.github.com/users/", user, "/repos"))
+
+	fmt.Println("Backup for user", user, "with", len(repos), "repositories")
+
+	var wg sync.WaitGroup
+	for _, repo := range repos {
+		wg.Add(1)
+		go func(routineRepo Repo) {
+			defer wg.Done()
+			updateRepo(backupDir, routineRepo)
+		}(repo)
+	}
+	wg.Wait()
+}
+
+// Get the two positional arguments user and backupdir
+func parseArgs() (string, string) {
+	flag.Parse()
+	args := flag.Args()
+	if len(args) != 2 {
+		fmt.Fprintf(os.Stderr, usage, os.Args[0])
+		os.Exit(1)
+	}
+	return args[0], args[1]
+}
+
+// Get repositories from Github.
+// Follow "next" links recursivly.
+func getRepos(url string) []Repo {
+	r, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer r.Body.Close()
+
+	if r.StatusCode >= 300 {
+		panic(fmt.Sprint("Request to ", url, "with bad status code ", r.StatusCode))
+	}
+
+	var repos []Repo
+	json.NewDecoder(r.Body).Decode(&repos)
+
+	firstLink := strings.Split(r.Header["Link"][0], ",")[0]
+	if strings.Contains(firstLink, "rel=\"next\"") {
+		urlInBrackets := strings.Split(firstLink, ";")[0]
+		return append(repos, getRepos(urlInBrackets[1:len(urlInBrackets)-1])...)
+	}
+
+	return repos
+}
+
+// Clone new repo or pull in existing repo
+func updateRepo(backupDir string, repo Repo) {
+	repoDir := path.Join(backupDir, repo.Name)
+
+	var cmd *exec.Cmd
+	if exists(repoDir) {
+		defer fmt.Println("Updated repository:", repo.Name)
+
+		cmd = exec.Command("git", "pull")
+		cmd.Dir = repoDir
+	} else {
+		defer fmt.Println("Cloned  repository:", repo.Name)
+
+		cmd = exec.Command("git", "clone", repo.GitUrl, repoDir)
+	}
+
+	err := cmd.Run()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+}
+
+// Check if a file or directory exists
+func exists(path string) bool {
+	_, err := os.Stat(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false
+		} else {
+			panic(err)
+		}
+	}
+	return true
+}
